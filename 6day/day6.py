6f36561d9b352a413437512b5365e1c49f4b09ed
dementeddr <rdbaumei@mtu.edu>
2019-12-15 13:32:32 -0500

    Day 6.2 done. Don't look at this code; it's gross.

--- 6day/day6.py
+++ 6day/day6.py
@@ -10,6 +10,8 @@ class Planet:
 		self.name = name
 		self.orbiters = []
 		self.orbit_val = 0
+		self.orbiting = None
+		self.height_from_body = -1
 
 	def orbiter_toString(self):
 
@@ -31,6 +33,21 @@ def find_orbit_vals(planet, depth):
 	return total+depth
 
 
+def find_body_height(planet, height):
+
+	print(f"{planet.name}: {planet.height_from_body}, {height}")
+
+	if planet.height_from_body > -1:
+		return planet.height_from_body + height
+
+	elif planet.name == 'COM':
+		return -1;
+
+	else: 
+		planet.height_from_body = height
+		return find_body_height(planet.orbiting, height+1)
+
+
 orbit_list = dict()
 
 with open("6day-input.txt", "r") as fp:
@@ -45,9 +62,22 @@ with open("6day-input.txt", "r") as fp:
 			orbit_list[bodies[1]] = Planet(bodies[1])
 
 		orbit_list[bodies[0]].orbiters.append(orbit_list[bodies[1]])
+		orbit_list[bodies[1]].orbiting = orbit_list[bodies[0]]
 
-		print(f"{orbit_list[bodies[0]].name} ){orbit_list[bodies[0]].orbiter_toString()}")
 
-print(f"Orbital Total: {find_orbit_vals(orbit_list['COM'], 0)}")
+for body in orbit_list.values():
+	orbiting = "   "
 
+	if body.orbiting is not None:
+		orbiting = body.orbiting.name
+
+	print(f"{orbiting} ) -{body.name}- ){body.orbiter_toString()}")
+
+print(f"Orbital Total: {find_orbit_vals(orbit_list['COM'], 0)}\n")
+
+find_body_height(orbit_list['SAN'].orbiting, 0)
+
+total_jumps = find_body_height(orbit_list['YOU'].orbiting, 0)
+
+print(f"Jumps to Santa: {total_jumps}")
 
