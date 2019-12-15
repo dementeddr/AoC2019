ace1f72b84c9c261945d3dbabf91be9c751ffe49
dementeddr <rdbaumei@mtu.edu>
2019-12-15 13:05:46 -0500

    Finished day 6.1

--- 6day/day6.py
+++ 6day/day6.py
@@ -1,16 +1,53 @@
 #! /bin/usr/env python3
 
 import sys
+from functools import reduce
 
 
-orbit_list = []
+class Planet:
+
+	def __init__(self, name="   "):
+		self.name = name
+		self.orbiters = []
+		self.orbit_val = 0
+
+	def orbiter_toString(self):
+
+		out_str = ""
+
+		for body in self.orbiters:
+			out_str = out_str + f" {body.name}"
+
+		return out_str
+
+
+def find_orbit_vals(planet, depth):
+
+	total = 0
+
+	for body in planet.orbiters:
+		total += find_orbit_vals(body, depth+1)
+
+	return total+depth
+
+
+orbit_list = dict()
 
 with open("6day-input.txt", "r") as fp:
 	
 	for line in fp:
-		orbit = line.split(')')
+		bodies = line[0:-1].split(')')
 
-		orbit_list.append((orbit[0], orbit[1][:-1]))
-		
-		print(orbit_list[-1])
+		if bodies[0] not in orbit_list:
+			orbit_list[bodies[0]] = Planet(bodies[0])
+
+		if bodies[1] not in orbit_list:
+			orbit_list[bodies[1]] = Planet(bodies[1])
+
+		orbit_list[bodies[0]].orbiters.append(orbit_list[bodies[1]])
+
+		print(f"{orbit_list[bodies[0]].name} ){orbit_list[bodies[0]].orbiter_toString()}")
+
+print(f"Orbital Total: {find_orbit_vals(orbit_list['COM'], 0)}")
+
 
