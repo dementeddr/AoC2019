ab32cc1277836137386e14d7a8bd84063b367a63
dementeddr <rdbaumei@mtu.edu>
2019-12-21 00:32:07 -0500

    Fixed day 7.2. I was quitting when the first amp finished, rather than the last.

--- 7day/day7-2.py
+++ 7day/day7-2.py
@@ -5,12 +5,12 @@ from intcomp2 import Status
 import itertools
 
 phase_vals = [5, 6, 7, 8, 9]
-filename = "test-input2.txt"
+filename = "7day-input.txt"
 phase_perms = list(itertools.permutations(phase_vals))
 amp_outputs = []
 max_output = 0
 
-phase_perms = [[9,7,8,5,6]]  #TEST INPUT PLEASE IGNORE
+#phase_perms = [[9,7,8,5,6]]  #TEST INPUT PLEASE IGNORE
 
 for phases in phase_perms:
 
@@ -25,7 +25,7 @@ for phases in phase_perms:
     prev = 0
     index = 0
 
-    while status == Status.OK or status == Status.INPUT_REQUIRED:
+    while status < 3:
         
         outs = amps[index].execute_tape([prev])
         status = outs[0]
@@ -34,6 +34,10 @@ for phases in phase_perms:
             prev = outs[1][0]
         
         print(f"{index} - {outs}")
+
+        if status == Status.FINISHED and index == 4: # If it's stupid and it works, it's not stupid
+            break
+
         index = (index + 1) % 5
     
     amp_outputs.append(prev)
