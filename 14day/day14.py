2bf009349849f37771372710b1b4f7e12cddbe31
Rex Baumeister <rex.baumeister@gmail.com>
2020-12-03 17:09:22 -0500

    Taking another crack at 14.1. Current solution works on 3 example inputs, but fails on 4th and the real input

--- 14day/day14.py
+++ 14day/day14.py
@@ -6,53 +6,62 @@ import queue
 filename = "14day-input.txt"
 
 if len(sys.argv) > 1:
-    filename = sys.argv[1]
+	filename = sys.argv[1]
 
 recipes = {}
-materials = {}
 ore_total = 0
-nodes = queue.SimpleQueue()
-#excess = {}
+materials = {}
+mat_q = []
 
 with open(filename, "r") as fp:
-    
-    for line in fp:
+	
+	for line in fp:
 
-        temp = line.split(',')
-        temp2 = temp[-1:][0].split(' ')[-2:]
-        product = str(temp2[1][:-1])
+		temp = line.split(',')
+		temp2 = temp[-1:][0].split(' ')[-2:]
+		product = str(temp2[1][:-1])
 
-        recipes[product] = [int(temp2[0]), {}]
-        
-        for chem in temp:
-            
-            if "=>" in chem:
-                chem = chem.split('=')[0]
+		recipes[product] = [int(temp2[0]), {}]
+		
+		for chem in temp:
+			
+			if "=>" in chem:
+				chem = chem.split('=')[0]
 
-            chem = chem.strip().split(' ')
-            #print(chem)
-    
-            recipes[product][1][chem[1]] = int(chem[0])
-            materials[chem[1]] = 0
-            #excess[chem[1]] = 0
-
+			chem = chem.strip().split(' ')
+	
+			recipes[product][1][chem[1]] = int(chem[0])
+			
+			materials[chem[1]] = 0
 
 
-materials["ORE"] = 0
-materials["FUEL"] = 0
-nodes.put("FUEL")
+materials["FUEL"] = 1
+mat_q.append("FUEL")
 
-while nodes.qsize() > 0:
+while len(mat_q) > 0:
 
-    prod = nodes.get()
+	prod = mat_q.pop(0)
+	if prod in mat_q:
+		continue
 
+	recipe = recipes[prod]
+	multiplier = 1
 
+	while recipe[0] * multiplier < materials[prod]:
+		multiplier += 1
 
+	for ingredient in recipe[1]:
+		
+		if ingredient == "ORE":
+			ore_total += recipe[1]["ORE"] * multiplier
 
+		else:
+			materials[ingredient] += recipe[1][ingredient] * multiplier
+			mat_q.append(ingredient)
 
+		materials[prod] = 0
 
-for key in sorted(recipes.keys()):
-    print(f"{key:5s} = {recipes[key]}")
+	print(f"####prod: {prod:<5} - {mat_q}")
+	print(materials)
 
-
-while 
+print(f"Total ORE needed: {ore_total}")
